06a32218ae03b6af948bbdbebad5dea0ccdf9bd1
quispepa <richardantonioquispe@gmail.com>
2024-09-13 03:19:54 +0200

    First commit without UML

---
+++ Week-4-Labs/src/Main.java
@@ -0,0 +1,12 @@
+import objects.Classroom;
+import objects.Student;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Main {
+  public static void main(String[] args) {
+    new Classroom();
+
+  }
+}
